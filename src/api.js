6be3b68208e13b22c5c80c2e1d4555f6e2c80a3c
Aditya B. <96489237+adityaaditya98@users.noreply.github.com>
2025-08-25 01:14:33 +0530

    baseURL

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-})
+    baseURL: "https://smtp-and-imap-backend.vercel.app",
 
+})
 const sendEmail = async (data) => {
     console.log("Sending email with data:", data);
     try {
